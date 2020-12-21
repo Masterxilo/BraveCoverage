610a1031f88afb0a05c08ce9d751166490e5d0ff
Paul Frischknecht <paulfrischknechtmx@gmail.com>
2020-12-21 17:25:14 +0100

    add double coverage

--- test/src/test_brave_coverage.cpp
+++ test/src/test_brave_coverage.cpp
@@ -4,6 +4,7 @@
 TEST(BraveCoverage, TestCaseOne) {
     Sum SumUnderTest;
     SumUnderTest.setAValue(10);
+    SumUnderTest.setAValue(10); // double execute this line
     SumUnderTest.setBValue(10);
 
     EXPECT_EQ(20, SumUnderTest.executeSum());
